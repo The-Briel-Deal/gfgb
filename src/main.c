3a995621a737cb409eb2639e29a59bfa2bbc5b37
Gabriel Ford <gabe@fordltc.net>
2025-11-29 17:02:28 -0500

    store shared state in struct gb_state

--- src/main.c
+++ src/main.c
@@ -1,23 +1,26 @@
-/* clear.c ... */
-
-/*
- * This example code creates an SDL window and renderer, and then clears the
- * window to a different color every frame, so you'll effectively get a window
- * that's smoothly fading between colors.
- *
- * This code is public domain. Feel free to use it for any purpose!
- */
-
+#include <SDL3/SDL_assert.h>
+#include <SDL3/SDL_stdinc.h>
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 /* We will use this renderer to draw into this window every frame. */
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
+struct gb_state {
+  SDL_Window *sdl_window;
+  SDL_Renderer *sdl_renderer;
+};
+
+void gb_state_init(struct gb_state *gb_state) {
+  gb_state->sdl_window = NULL;
+  gb_state->sdl_renderer = NULL;
+}
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+  struct gb_state *gb_state = SDL_malloc(sizeof(struct gb_state));
+  *appstate = gb_state;
+  SDL_assert(appstate != NULL);
+  gb_state_init(*appstate);
   SDL_SetAppMetadata("Example Renderer Clear", "1.0",
                      "com.example.renderer-clear");
 
@@ -27,11 +30,12 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   }
 
   if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480,
-                                   SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+                                   SDL_WINDOW_RESIZABLE, &gb_state->sdl_window,
+                                   &gb_state->sdl_renderer)) {
     SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
-  SDL_SetRenderLogicalPresentation(renderer, 640, 480,
+  SDL_SetRenderLogicalPresentation(gb_state->sdl_renderer, 640, 480,
                                    SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
   return SDL_APP_CONTINUE; /* carry on with the program! */
@@ -39,13 +43,13 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+  struct gb_state *gb_state = appstate;
   switch (event->type) {
   case SDL_EVENT_QUIT: return SDL_APP_SUCCESS;
   case SDL_EVENT_WINDOW_RESIZED:
-    SDL_SetRenderLogicalPresentation(renderer, event->window.data1,
-                                     event->window.data2,
+    SDL_SetRenderLogicalPresentation(gb_state->sdl_renderer,
+                                     event->window.data1, event->window.data2,
                                      SDL_LOGICAL_PRESENTATION_LETTERBOX);
-
     break;
   }
 
@@ -54,6 +58,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
+  struct gb_state *gb_state = appstate;
   /* convert from milliseconds to seconds. */
   const double now = ((double)SDL_GetTicks()) / 1000.0;
 
@@ -63,21 +68,21 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
   const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
   const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
   SDL_SetRenderDrawColorFloat(
-      renderer, red, green, blue,
+      gb_state->sdl_renderer, red, green, blue,
       SDL_ALPHA_OPAQUE_FLOAT); /* new color, full alpha. */
 
   /* clear the window to the draw color. */
-  SDL_RenderClear(renderer);
+  SDL_RenderClear(gb_state->sdl_renderer);
 
   SDL_SetRenderDrawColorFloat(
-      renderer, 1.0, 1.0, 1.0,
+      gb_state->sdl_renderer, 1.0, 1.0, 1.0,
       SDL_ALPHA_OPAQUE_FLOAT); /* new color, full alpha. */
 
   struct SDL_FRect rect = {.x = 100.0, .y = 100.0, .h = 200.0, .w = 200.0};
-  SDL_RenderFillRect(renderer, &rect);
+  SDL_RenderFillRect(gb_state->sdl_renderer, &rect);
 
   /* put the newly-cleared rendering on the screen. */
-  SDL_RenderPresent(renderer);
+  SDL_RenderPresent(gb_state->sdl_renderer);
 
   return SDL_APP_CONTINUE; /* carry on with the program! */
 }
@@ -85,4 +90,5 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
   /* SDL will clean up the window/renderer for us. */
+  SDL_free(appstate);
 }
