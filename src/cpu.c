80004b2f89888384bd047b310321a480999963ba
Gabriel Ford <gabe@fordltc.net>
2025-12-07 13:08:10 -0500

    add execute impl for ld_r16_imm16

--- src/cpu.c
+++ src/cpu.c
@@ -2,6 +2,7 @@
 #include "common.h"
 
 #include <assert.h>
+#include <stdint.h>
 
 #define R16_PARAM(r)                                                           \
   (struct inst_param) { .type = R16, .r16 = r }
@@ -20,6 +21,42 @@ static inline uint16_t next16(struct gb_state *gb_state) {
   uint16_t val = read_mem16(gb_state, gb_state->regs.pc);
   gb_state->regs.pc += 2;
   return val;
+}
+
+static inline uint16_t get_r16(struct gb_state *gb_state, uint8_t r16) {
+  switch (r16) {
+  case 0: // bc
+    return COMBINED_REG(gb_state->regs, b, c);
+  case 1: // de
+    return COMBINED_REG(gb_state->regs, d, e);
+  case 2: // hl
+    return COMBINED_REG(gb_state->regs, h, l);
+  case 3: // sp
+    return gb_state->regs.sp;
+  default:
+    // bc, de, hl, and sp are the only valid r16 registers.
+    exit(1);
+  }
+}
+
+static inline void set_r16(struct gb_state *gb_state, uint8_t r16,
+                           uint16_t val) {
+  switch (r16) {
+  case 0: // bc
+    gb_state->regs.b = (0x00FF & val) >> 0;
+    gb_state->regs.c = (0xFF00 & val) >> 8;
+  case 1: // de
+    gb_state->regs.d = (0x00FF & val) >> 0;
+    gb_state->regs.e = (0xFF00 & val) >> 8;
+  case 2: // hl
+    gb_state->regs.h = (0x00FF & val) >> 0;
+    gb_state->regs.l = (0xFF00 & val) >> 8;
+  case 3: // sp
+    gb_state->regs.sp = val;
+  default:
+    // bc, de, hl, and sp are the only valid r16 registers.
+    exit(1);
+  }
 }
 
 struct inst fetch(struct gb_state *gb_state) {
@@ -44,7 +81,30 @@ struct inst fetch(struct gb_state *gb_state) {
                curr_byte);
   NOT_IMPLEMENTED("Instruction not implemented.");
 }
-void execute(struct gb_state *gb_state, struct inst inst) {}
+void execute(struct gb_state *gb_state, struct inst inst) {
+  switch (inst.type) {
+  case NOP: return;
+  case LD:
+    switch (inst.p1.type) {
+    case R16: {
+      uint8_t reg_dest = inst.p1.r16;
+      switch (inst.p2.type) {
+      case IMM16: {
+        uint16_t src_val = inst.p2.imm16;
+        set_r16(gb_state, reg_dest, src_val);
+        return;
+      }
+      default: break;
+      }
+    }
+    default: break;
+    }
+    break;
+  default: break;
+  }
+  NOT_IMPLEMENTED(
+      "`execute()` called with `inst.type` that isn't implemented.");
+}
 
 #ifdef RUN_TESTS
 
