b53b08134d7f8e9caa9ca1db3f12aa7f73a091de
Gabriel Ford <gabe@fordltc.net>
2025-12-13 22:26:00 -0500

    get test sprite displaying

--- progs/simple_sprite/simple_sprite.asm
+++ progs/simple_sprite/simple_sprite.asm
@@ -1,9 +1,25 @@
 SECTION "Header", ROM0[$100]
-  jp SimpleSprite
-  ds $150 - @, 0
+  JP SimpleSprite
+  DS $150 - @, 0
+
 SimpleSprite:
-  ld HL, $C000
-  ld a, $3D
-  ld [HL+], a
-  ld a, $4D
-  ld [HL+], a
+  LD A, $1B
+  LD [$FF47], A
+
+  LD D, 16
+  LD HL, $8000
+  LD BC, DoggoSprite
+  .loop:
+  LD A, [BC]
+  LD [HLI], A
+  INC BC
+  DEC D
+  JR NZ, .loop
+  .loop_end:
+  .forever_loop:
+  NOP
+  JP .forever_loop
+
+DoggoSprite:
+  DB 0x00, 0x00, 0x00, 0x00, 0x30, 0x50, 0xe9, 0x89, 0x3e, 0x02, 0x24, 0x18, 0x24, 0x24, 0x00, 0x00
+
