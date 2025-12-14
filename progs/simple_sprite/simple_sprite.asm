9df1e5c5d40de7ec1ed45627da68d4848e04540f
Gabriel Ford <gabe@fordltc.net>
2025-12-14 17:25:38 -0500

    get simple_sprite prog working with emulicious

--- progs/simple_sprite/simple_sprite.asm
+++ progs/simple_sprite/simple_sprite.asm
@@ -1,13 +1,24 @@
+INCLUDE "hardware.inc"
+
 SECTION "Header", ROM0[$100]
   JP SimpleSprite
   DS $150 - @, 0
 
 SimpleSprite:
-  LD A, $1B
-  LD [$FF47], A
+	; Shut down audio circuitry
+	ld a, 0
+	ld [rNR52], a
+  .wait_for_vblank:
+	ld a, [rLY]
+	cp 144
+	jp c, .wait_for_vblank
+
+	; Turn the LCD off
+	ld a, 0
+	ld [rLCDC], a
 
   LD D, 16
-  LD HL, $8000
+  LD HL, $9000
   LD BC, DoggoSprite
   .loop:
   LD A, [BC]
@@ -15,10 +26,14 @@ SimpleSprite:
   INC BC
   DEC D
   JR NZ, .loop
-  .loop_end:
-  .forever_loop:
-  NOP
-  JP .forever_loop
+  ; Turn the LCD on
+  ld a, LCDCF_ON | LCDCF_BGON
+  ld [rLCDC], a
+  ; During the first (blank) frame, initialize display registers
+  ld a, %11100100
+  ld [rBGP], a
+  .done:
+  jp .done
 
 DoggoSprite:
   INCBIN "doggo.bin"
