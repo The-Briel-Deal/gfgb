2a30667117ae1691f950f2c1b0ec4cfd94a70c8f
Gabriel Ford <gabe@fordltc.net>
2026-01-08 19:48:20 -0500

    Get `test_gb_tile_to_msb2()` working

--- src/ppu.c
+++ src/ppu.c
@@ -6,12 +6,16 @@ void gb_tile_to_msb2(uint8_t *gb_tile_in, uint8_t *msb2_tile_out) {
   for (int i = 0; i < 8; i++) {
     uint8_t b1 = gb_tile_in[(i * 2) + 0];
     uint8_t b2 = gb_tile_in[(i * 2) + 1];
-    msb2_tile_out[(i * 2) + 0] = 0;
-    msb2_tile_out[(i * 2) + 1] = 0;
-    for (int j = 0; j < 8; j++) {
-      msb2_tile_out[(i * 2) + (j / 4)] |= ((1 << j) & b1);
-      msb2_tile_out[(i * 2) + (j / 4)] |= (((1 << j) & b2) >> 1);
-    }
+    msb2_tile_out[(i * 2) + 0] =
+        ((((b2 >> 7) & 1) << 7) | (((b1 >> 7) & 1) << 6) |
+         (((b2 >> 6) & 1) << 5) | (((b1 >> 6) & 1) << 4) |
+         (((b2 >> 5) & 1) << 3) | (((b1 >> 5) & 1) << 2) |
+         (((b2 >> 4) & 1) << 1) | (((b1 >> 4) & 1) << 0));
+    msb2_tile_out[(i * 2) + 1] =
+        ((((b2 >> 3) & 1) << 7) | (((b1 >> 3) & 1) << 6) |
+         (((b2 >> 2) & 1) << 5) | (((b1 >> 2) & 1) << 4) |
+         (((b2 >> 1) & 1) << 3) | (((b1 >> 1) & 1) << 2) |
+         (((b2 >> 0) & 1) << 1) | (((b1 >> 0) & 1) << 0));
   }
 }
 #ifdef RUN_PPU_TESTS
@@ -32,8 +36,8 @@ void test_gb_tile_to_msb2() {
   gb_tile_to_msb2(gb_tile_in, msb2_tile_result);
   for (int i = 0; i < 16; i++) {
     if (msb2_tile_expect[i] != msb2_tile_result[i]) {
-      SDL_Log("byte i=%d of msb2 result (%.8b) is not equal to result (%.8b)\n", i,
-             msb2_tile_result[i], msb2_tile_expect[i]);
+      SDL_Log("byte i=%d of msb2 result (%.8b) is not equal to result (%.8b)\n",
+              i, msb2_tile_result[i], msb2_tile_expect[i]);
       abort();
     }
   }
