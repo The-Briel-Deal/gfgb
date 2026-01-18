af0c041a1c7b8b677b7180fa344ff1b05ec5035a
Gabriel Ford <gabe@fordltc.net>
2026-01-18 13:26:19 -0500

    get all sst cases loaded into a dataclass

--- test/sst/sst_test.py
+++ test/sst/sst_test.py
@@ -1,10 +1,41 @@
-import sys
+from dataclasses import dataclass
+import json
 import pathlib
 import pytest
+import sys
+
+from typing import Any, List, Optional, Union
+
 # This is a janky way to make sure that my cython module is in path. I'll figure out a better solution later.
 sys.path.append("build")
 
 import gfgb_py
+
+
+@dataclass
+class StateSnapshot:
+  a: int
+  b: int
+  c: int
+  d: int
+  e: int
+  f: int
+  h: int
+  l: int
+  pc: int
+  sp: int
+  ime: int
+  ram: List[List[int]]
+  ei: Optional[int] = None
+  ie: Optional[int] = None
+
+
+@dataclass
+class SSTCase:
+  name: str
+  initial: StateSnapshot
+  final: StateSnapshot
+  cycles: List[List[Union[int, str]]]
 
 
 sst_test_dir = pathlib.Path(__file__).parent / "test_json/"
@@ -13,6 +44,15 @@ test_files = list(sst_test_dir.iterdir())
 assert len(test_files) == 500
 
 
-@pytest.mark.parametrize("test_file", test_files)
-def test_single_step(test_file: pathlib.Path):
-  assert test_file.is_file()
+@pytest.mark.parametrize("test_file_path", test_files)
+def test_single_step(test_file_path: pathlib.Path):
+  assert test_file_path.is_file()
+  test_file = test_file_path.open()
+  test_data: list[dict[str, Any]] = json.load(test_file)
+  for case in test_data:
+    sst_case = SSTCase(
+        name=case["name"],
+        initial=StateSnapshot(**case["initial"]),
+        final=StateSnapshot(**case["final"]),
+        cycles=case["cycles"],
+    )
