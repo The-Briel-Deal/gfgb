fe6f87bfb57c05070db4e550a3261664b2ae63cb
Gabriel Ford <gabe@fordltc.net>
2026-01-17 19:16:46 -0500

    remove old headers and bindings created with c2python, switch to cython

--- test/build_headers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-clang2py -l ./build/libgfgb.so ./src/common.h -o ./test/headers/common.py --clang-args="-I/usr/lib/clang/21/include/"
-clang2py -l ./build/libgfgb.so ./src/cpu.h -o ./test/headers/cpu.py --clang-args="-I/usr/lib/clang/21/include/"
-clang2py -l ./build/libgfgb.so ./src/disassemble.h -o ./test/headers/disassemble.py --clang-args="-I/usr/lib/clang/21/include/"
-clang2py -l ./build/libgfgb.so ./src/ppu.h -o ./test/headers/ppu.py --clang-args="-I/usr/lib/clang/21/include/"
