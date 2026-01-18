450d876bb1ab4ebb0e42664d3dbe8c86a51f8f5e
Gabriel Ford <gabe@fordltc.net>
2026-01-18 09:30:20 -0500

    switch gfgb.pyx to pure python

--- py/gfgb.pyx
+++ py/gfgb.pyx
@@ -1,10 +1,12 @@
-cimport gfgb 
+from cython.cimports import gfgb  # type: ignore
+
 
 def main():
   gb_state = gfgb.gb_state_alloc()
   gfgb.gb_state_init(gb_state)
   gfgb.set_r8(gb_state, gfgb.R8_B, 10)
   print(gfgb.get_r8(gb_state, gfgb.R8_B))
+
 
 if __name__ == "__main__":
   main()
