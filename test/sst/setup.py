f2e89c8a6eba62f501414a813882864020fea344
Gabriel Ford <gabe@fordltc.net>
2026-01-18 00:50:11 -0500

    add back `setup.py` file so that source files can be copied to test root

---
+++ test/sst/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+
+from setuptools import Extension, setup
+from setuptools.command.sdist import sdist
+
+
+class SDist(sdist):
+
+  def make_release_tree(self, base_dir, files) -> None:
+    self.mkpath(base_dir)
+    self.copy_tree(
+        infile="../../src/",
+        outfile=str((pathlib.Path(base_dir) / "c_src/").absolute()),
+    )
+    return super().make_release_tree(base_dir, files)
+
+
+setup(
+    ext_modules=[
+        Extension(
+            name="gfgb_py",
+            sources=["src/*.pyx"],
+            depends=["src/gfgb.pxd"],
+            include_dirs=["c_src/"],
+        ),
+    ],
+    cmdclass={
+        "sdist": SDist,
+    },
+)
--- test/sst/src/sst_json_tests.pyx
+++ test/sst/src/sst_json_tests.pyx
@@ -2,8 +2,7 @@ cimport gfgb
 
 def main():
   gb_state = gfgb.gb_state_alloc()
-  print("Hello World", gb_state)
-
+  gfgb.gb_state_init(gb_state)
 
 if __name__ == "__main__":
   main()
