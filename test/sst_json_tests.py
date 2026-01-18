9519287edeb21bb6873a81950fa1fb0ffd3e033e
Gabriel Ford <gabe@fordltc.net>
2026-01-17 19:35:07 -0500

    setup basic cython project

--- test/sst_json_tests.py
+++ test/sst_json_tests.py
@@ -1,14 +1,5 @@
-import ctypes
-
-class GBState(ctypes.Structure):
-  pass
-
-
 def main():
-  gfgb = ctypes.CDLL("./build/libgfgb.so")
-  gfgb.gb_state_alloc.restype = ctypes.POINTER(GBState)
-  gb_state = gfgb.gb_state_alloc()
-  gfgb.gb_state_init(gb_state)
+  print("Hello World")
 
 
 if __name__ == "__main__":
