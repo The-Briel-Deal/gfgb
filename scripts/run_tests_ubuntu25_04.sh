4aa169cae18c8a3fcf2f8ae61059f11978008c19
Gabriel Ford <gabe@fordltc.net>
2026-03-06 17:41:32 -0500

    fix sdl version issue in github actions test container

--- scripts/run_tests_ubuntu25_04.sh
+++ scripts/run_tests_ubuntu25_04.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-apt update
-apt upgrade
-apt install meson gcc cython3 python3
+set -e
+
+export TZ=America/New_York
+export DEBIAN_FRONTEND=noninteractive
+
+cd /gb_emu/
+
+# There are a ton of deps required for building SDL3, so I just make sure we
+# populate deb-src sources so that we can use apt-get to install build-deps.
+sed -Ei 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
+
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install meson gcc python3 python3-pip python3-venv
+apt-get -y build-dep libsdl3-dev
+
+python3 -m venv /venv
+source /venv/bin/activate
+pip install Cython==3.2.4
+
 meson setup /build
 meson test -C/build
