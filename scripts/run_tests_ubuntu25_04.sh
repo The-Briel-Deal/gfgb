c716249738688968edb5820d83655ad52d34c406
Gabriel Ford <gabe@fordltc.net>
2026-03-07 16:27:18 -0500

    change test script to run directly on runner

--- scripts/run_tests_ubuntu25_04.sh
+++ scripts/run_tests_ubuntu25_04.sh
@@ -9,16 +9,15 @@ cd /gb_emu/
 
 # There are a ton of deps required for building SDL3, so I just make sure we
 # populate deb-src sources so that we can use apt-get to install build-deps.
-sed -Ei 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
+# sed -Ei 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
 
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install meson gcc git python3 python3-pip python3-venv
-apt-get -y build-dep libsdl3-dev
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install gcc-14 git python3 python3-pip python3-venv
 
 python3 -m venv /venv
 source /venv/bin/activate
-pip install Cython==3.2.4 pytest==8.4.2 pytest-tap==3.5
+pip install Cython==3.2.4 pytest==8.4.2 pytest-tap==3.5 meson==1.10.1 meson-python==0.19.0
 
 meson setup /build -Dpython_install=/venv/bin/python
 meson test -C/build --verbose
