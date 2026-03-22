d53b48400c2d1c59d90f32b565908f756bf76b43
Gabriel Ford <gabe@fordltc.net>
2026-03-22 12:33:59 -0400

    add header guard to timing.h

--- src/timing.h
+++ src/timing.h
@@ -1,5 +1,10 @@
+#ifndef GB_TIMING_H
+#define GB_TIMING_H
+
 #include <stdint.h>
 
 typedef struct gb_state gb_state_t;
 
 void gb_spend_mcycles(gb_state_t *gb_state, uint64_t n);
+
+#endif // GB_TIMING_H
