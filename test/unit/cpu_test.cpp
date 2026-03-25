ca4d16df671681d6e350b0578ee48839c0491f3f
Gabriel Ford <gabe@fordltc.net>
2026-03-25 17:45:31 -0400

    move all unit tests to using Catch2

--- test/unit/cpu_test.cpp
+++ test/unit/cpu_test.cpp
@@ -1,7 +1,7 @@
 #include "common.h"
 #include "test_common.h"
 
-void test_fetch() {
+TEST_CASE("Fetch CPU Instruction", "[CPU]") {
   struct gb_state gb_state;
   struct inst     inst;
 
@@ -12,40 +12,40 @@ void test_fetch() {
   gb_state.saved.ram.rom0[0x101] = 0xCD;
   gb_state.saved.ram.rom0[0x102] = 0xAB;
   inst                           = fetch(&gb_state);
-  GB_assert(inst.type == LD);
-  GB_assert(inst.p1.type == R16);
-  GB_assert(inst.p1.r16 == R16_HL);
-  GB_assert(inst.p2.type == IMM16);
-  GB_assert(inst.p2.imm16 == 0xABCD);
+  CHECK(inst.type == LD);
+  CHECK(inst.p1.type == R16);
+  CHECK(inst.p1.r16 == R16_HL);
+  CHECK(inst.p2.type == IMM16);
+  CHECK(inst.p2.imm16 == 0xABCD);
 
   gb_state.saved.ram.rom0[0x103] = 0b00010010;
   inst                           = fetch(&gb_state);
-  GB_assert(inst.type == LD);
-  GB_assert(inst.p1.type == R16_MEM);
-  GB_assert(inst.p1.r16 == R16_DE);
-  GB_assert(inst.p2.type == R8);
-  GB_assert(inst.p2.r8 == R8_A);
+  CHECK(inst.type == LD);
+  CHECK(inst.p1.type == R16_MEM);
+  CHECK(inst.p1.r16 == R16_DE);
+  CHECK(inst.p2.type == R8);
+  CHECK(inst.p2.r8 == R8_A);
 
   gb_state.saved.ram.rom0[0x104] = 0b00011010;
   inst                           = fetch(&gb_state);
-  GB_assert(inst.type == LD);
-  GB_assert(inst.p1.type == R8);
-  GB_assert(inst.p1.r8 == R8_A);
-  GB_assert(inst.p2.type == R16_MEM);
-  GB_assert(inst.p2.r16 == R16_DE);
+  CHECK(inst.type == LD);
+  CHECK(inst.p1.type == R8);
+  CHECK(inst.p1.r8 == R8_A);
+  CHECK(inst.p2.type == R16_MEM);
+  CHECK(inst.p2.r16 == R16_DE);
 
   gb_state.saved.ram.rom0[0x105] = 0b00001000;
   gb_state.saved.ram.rom0[0x106] = 0x34;
   gb_state.saved.ram.rom0[0x107] = 0x12;
   inst                           = fetch(&gb_state);
-  GB_assert(inst.type == LD);
-  GB_assert(inst.p1.type == IMM16_MEM);
-  GB_assert(inst.p1.imm16 == 0x1234);
-  GB_assert(inst.p2.type == R16);
-  GB_assert(inst.p2.r16 == R16_SP);
+  CHECK(inst.type == LD);
+  CHECK(inst.p1.type == IMM16_MEM);
+  CHECK(inst.p1.imm16 == 0x1234);
+  CHECK(inst.p2.type == R16);
+  CHECK(inst.p2.r16 == R16_SP);
 }
 
-void test_execute_load() {
+TEST_CASE("Fetch CPU Execute LD", "[CPU]") {
   gb_state_t gb_state;
   gb_state_init(&gb_state);
   inst_t inst;
@@ -57,7 +57,7 @@ void test_execute_load() {
       .p2   = IMM16_PARAM(452),
   };
   execute(&gb_state, inst);
-  GB_assert(get_r16(&gb_state, R16_BC) == 452);
+  REQUIRE(get_r16(&gb_state, R16_BC) == 452);
 
   // Load reg A into addr in reg BC
   inst = inst_t{
@@ -68,7 +68,7 @@ void test_execute_load() {
   set_r16(&gb_state, R16_BC, 0xC000);
   gb_state.saved.regs.a = 42;
   execute(&gb_state, inst);
-  GB_assert(gb_read_mem(&gb_state, 0xC000) == 42);
+  REQUIRE(gb_read_mem(&gb_state, 0xC000) == 42);
 
   // Load contents of addr in reg BC into reg A
   inst = inst_t{
@@ -78,7 +78,7 @@ void test_execute_load() {
   };
   gb_write_mem(&gb_state, 0xC000, 134);
   execute(&gb_state, inst);
-  GB_assert(gb_state.saved.regs.a == 134);
+  REQUIRE(gb_state.saved.regs.a == 134);
 
   // Load contents of addr in reg HL into reg A and increment the pointer.
   inst = inst_t{
@@ -89,8 +89,8 @@ void test_execute_load() {
   set_r16(&gb_state, R16_HL, 0xC000);
   gb_write_mem(&gb_state, 0xC000, 134);
   execute(&gb_state, inst);
-  GB_assert(get_r16(&gb_state, R16_HL) == 0xC001);
-  GB_assert(gb_state.saved.regs.a == 134);
+  REQUIRE(get_r16(&gb_state, R16_HL) == 0xC001);
+  REQUIRE(gb_state.saved.regs.a == 134);
   // Then load contents of reg A into the addr in reg HL.
   inst = inst_t{
       .type = LD,
@@ -99,8 +99,8 @@ void test_execute_load() {
   };
   set_r8(&gb_state, R8_A, 21);
   execute(&gb_state, inst);
-  GB_assert(gb_read_mem(&gb_state, 0xC001) == 21);
-  GB_assert(get_r16(&gb_state, R16_HL) == 0xC000);
+  REQUIRE(gb_read_mem(&gb_state, 0xC001) == 21);
+  REQUIRE(get_r16(&gb_state, R16_HL) == 0xC000);
 
   // Load stack pointer into addr at IMM16
   inst = inst_t{
@@ -110,11 +110,11 @@ void test_execute_load() {
   };
   set_r16(&gb_state, R16_SP, 0xD123);
   execute(&gb_state, inst);
-  GB_assert(gb_state.saved.regs.sp == 0xD123);
-  GB_assert((gb_read_mem(&gb_state, 0xC010) | (gb_read_mem(&gb_state, 0xC011) << 8)) == 0xD123);
+  REQUIRE(gb_state.saved.regs.sp == 0xD123);
+  REQUIRE((gb_read_mem(&gb_state, 0xC010) | (gb_read_mem(&gb_state, 0xC011) << 8)) == 0xD123);
 }
 
-void test_stack_ops() {
+TEST_CASE("Stack Operations", "[CPU]") {
   gb_state_t gb_state;
   gb_state_init(&gb_state);
 
@@ -128,7 +128,7 @@ void test_stack_ops() {
   assert_eq(pop16(&gb_state), 0x1234);
 }
 
-void test_execute_call_ret() {
+TEST_CASE("Call and Return", "[CPU]") {
   gb_state_t gb_state;
   gb_state_init(&gb_state);
   assert_eq(gb_state.saved.regs.sp, 0xE000);
@@ -140,20 +140,4 @@ void test_execute_call_ret() {
   execute(&gb_state, inst_t{.type = RET, .p1 = VOID_PARAM, .p2 = VOID_PARAM});
   assert_eq(gb_state.saved.regs.sp, 0xE000);
   assert_eq(gb_state.saved.regs.pc, 0x0190);
-}
-
-#define TEST_CASE(name)                                                                                                \
-  {                                                                                                                    \
-    LogInfo("running `test_%s()`", #name);                                                                             \
-    test_##name();                                                                                                     \
-  }
-
-int main() {
-  LogInfo("Starting CPU tests.");
-  TEST_CASE(fetch);
-  TEST_CASE(execute_load);
-  TEST_CASE(execute_call_ret);
-  TEST_CASE(stack_ops);
-  LogInfo("CPU tests succeeded.");
-  SDL_Quit();
 }
--- test/unit/test_common.h
+++ test/unit/test_common.h
@@ -5,71 +5,38 @@
 #include "catch2/catch_test_macros.hpp"
 
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#ifdef __cplusplus
 #include <type_traits>
-#endif
 
-#define LOC __FILE__, __LINE__, __func__
-
-static inline void assert_int_eq(const char *fname, int lineno, const char *fxname, int v1, int v2) {
-  if (v1 != v2) {
-    fprintf(stderr, "test assertion failed:\n");
-    fprintf(stderr, "%s@%d - %s(): v1 (%d) != v2 (%d)\n", fname, lineno, fxname, v1, v2);
-    abort();
-  }
+static inline void assert_int_eq(int v1, int v2) {
+  CHECKED_IF(v1 != v2) { FAIL(); }
 }
-static inline void assert_uint8_eq_hex(const char *fname, int lineno, const char *fxname, uint8_t v1, uint8_t v2) {
-  if (v1 != v2) {
-    fprintf(stderr, "test assertion failed:\n");
-    fprintf(stderr, "%s@%d - %s(): v1 (0x%.2x) != v2 (0x%.2x)\n", fname, lineno, fxname, v1, v2);
-    abort();
-  }
+static inline void assert_uint8_eq_hex(uint8_t v1, uint8_t v2) {
+  CHECKED_IF(v1 != v2) { FAIL(); }
 }
-static inline void assert_uint16_eq_hex(const char *fname, int lineno, const char *fxname, uint16_t v1, uint16_t v2) {
-  if (v1 != v2) {
-    fprintf(stderr, "test assertion failed:\n");
-    fprintf(stderr, "%s@%d - %s(): v1 (0x%.4x) != v2 (0x%.4x)\n", fname, lineno, fxname, v1, v2);
-    abort();
-  }
+static inline void assert_uint16_eq_hex(uint16_t v1, uint16_t v2) {
+  CHECKED_IF(v1 != v2) { FAIL(); }
 }
-static inline void assert_str_eq(const char *fname, int lineno, const char *fxname, char *v1, char *v2) {
-  if (strcmp(v1, v2)) {
-    fprintf(stderr, "test assertion failed:\n");
-    fprintf(stderr, "%s@%d - %s(): v1 (%s) != v2 (%s)\n", fname, lineno, fxname, v1, v2);
-    abort();
-  }
+static inline void assert_str_eq(char *v1, char *v2) {
+  CHECKED_IF(strcmp(v1, v2)) { FAIL("v1:\n" << v1 << "\nv2:\n" << v2); }
 }
-#ifdef __cplusplus
 template <typename T1, typename T2> void assert_eq(T1 v1, T2 v2) {
   if constexpr (std::is_same_v<T1, uint8_t>) {
-    return assert_uint8_eq_hex(LOC, v1, (T1)v2);
+    return assert_uint8_eq_hex(v1, (T1)v2);
   } else if constexpr (std::is_same_v<T1, unsigned char>) {
-    return assert_uint8_eq_hex(LOC, v1, (T1)v2);
+    return assert_uint8_eq_hex(v1, (T1)v2);
   } else if constexpr (std::is_same_v<T1, uint16_t>) {
-    return assert_uint16_eq_hex(LOC, v1, (T1)v2);
+    return assert_uint16_eq_hex(v1, (T1)v2);
   } else if constexpr (std::is_same_v<T1, uint32_t>) {
-    return assert_int_eq(LOC, v1, (T1)v2);
+    return assert_int_eq(v1, (T1)v2);
   } else if constexpr (std::is_same_v<T1, int>) {
-    return assert_int_eq(LOC, v1, (T1)v2);
+    return assert_int_eq(v1, (T1)v2);
   } else if constexpr (std::is_same_v<T1, char *>) {
-    return assert_str_eq(LOC, v1, (T1)v2);
+    return assert_str_eq(v1, (T1)v2);
   } else {
     static_assert(false, "Unsupported type for assert_eq");
   }
 }
-
-#else
-#define assert_eq(v1, v2)                                                                                              \
-  _Generic((v1),                                                                                                       \
-      uint8_t: assert_uint8_eq_hex,                                                                                    \
-      uint16_t: assert_uint16_eq_hex,                                                                                  \
-      uint32_t: assert_int_eq,                                                                                         \
-      int: assert_int_eq,                                                                                              \
-      char *: assert_str_eq)(LOC, v1, v2)
-#endif
-
 #endif
