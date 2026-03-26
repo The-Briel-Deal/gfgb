e4dc1054435551cb7c024bb969ed4ea2b7c71be2
Gabriel Ford <gabe@fordltc.net>
2026-03-26 14:09:14 -0400

    check all parsed_header fields in test

--- test/unit/mbc_test.cpp
+++ test/unit/mbc_test.cpp
@@ -16,5 +16,9 @@ TEST_CASE("Parse MBC1 512kb ROM Header", "[mbc]") {
   uint8_t header[0x50];
   fread(header, 1, 0x50, f);
   gb_cart_header_t parsed_header = gb_parse_cart_header(header);
-  REQUIRE(parsed_header.mbc_type == GB_MBC1);
+  CHECK(parsed_header.mbc_type == GB_MBC1);
+  CHECK(parsed_header.has_battery == false);
+  CHECK(parsed_header.has_ram == false);
+  CHECK(parsed_header.has_rumble == false);
+  CHECK(parsed_header.has_rtc == false);
 }
