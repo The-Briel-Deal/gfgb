bc02ce474c300638c2bd2a14b42ec6c655844f09
Gabriel Ford <gabe@fordltc.net>
2026-04-21 11:55:38 -0400

    set BGP and tilemap block

--- test/gf/layer_priority/src/intro.asm
+++ test/gf/layer_priority/src/intro.asm
@@ -4,8 +4,13 @@ INCLUDE "defines.inc"
 SECTION "Intro", ROMX
 
 Intro::
+  ; Set BG Tile Data block to $8000 and tile map to $9C00
   ld hl, rLCDC
   set 4, [hl]
+  set 3, [hl]
+
+  ld hl, rBGP
+  ld [hl], 0b00011011
 
   ld hl, $8000
   ld bc, $10
