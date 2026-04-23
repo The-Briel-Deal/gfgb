1acac28087066098a092fae89dc39f65aac6ca77
Gabriel Ford <gabe@fordltc.net>
2026-04-22 20:40:19 -0400

    correct name of ninja to ninja-build

--- scripts/run_tests_ubuntu25_04.sh
+++ scripts/run_tests_ubuntu25_04.sh
@@ -12,7 +12,7 @@ cd /gb_emu/
 sed -Ei 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
 
 apt-get -y update
-apt-get -y install ninja gcc git python3 python3-pip python3-venv
+apt-get -y install ninja-build gcc git python3 python3-pip python3-venv
 apt-get -y build-dep libsdl3-dev
 
 python3 -m venv /venv
