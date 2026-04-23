7c2fb5858d4fd7d0624edb1e4b841ca8ece65b1d
Gabriel Ford <gabe@fordltc.net>
2026-04-23 19:17:49 -0400

    got some audio playing from ch1

--- test/gf/apu_ch1/src/intro.asm
+++ test/gf/apu_ch1/src/intro.asm
@@ -5,4 +5,18 @@ INCLUDE "defines.inc"
 
 SECTION "Intro", ROMX
 Intro::
-  jr @
+  ld a, %10001111
+  ldh [rAUDENA], a            ; Turn on sound.
+  ld a, $ff
+  ldh [rAUDVOL], a            ; Full volume, both channels on.
+  ldh [rAUDTERM], a           ; All sounds to all terminals.
+  ld a, 0b1000_0000
+  ldh [rNR11], a
+  ld a, 0b1111_1010
+  ldh [rNR12], a
+  ld a, 0b1111_1010
+  ldh [rNR13], a
+Trigger:
+  ld a, $ff
+  ldh [rNR14], a
+  jr Trigger
