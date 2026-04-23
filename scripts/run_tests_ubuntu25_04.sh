43567c78cf458242b9514321daed5d2dd1324bdc
Gabriel Ford <gabe@fordltc.net>
2026-04-22 20:33:03 -0400

    use meson 1.10.0 in CI

--- scripts/run_tests_ubuntu25_04.sh
+++ scripts/run_tests_ubuntu25_04.sh
@@ -12,12 +12,12 @@ cd /gb_emu/
 sed -Ei 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
 
 apt-get -y update
-apt-get -y install meson gcc git python3 python3-pip python3-venv
+apt-get -y install gcc git python3 python3-pip python3-venv
 apt-get -y build-dep libsdl3-dev
 
 python3 -m venv /venv
 source /venv/bin/activate
-pip install Cython==3.2.4 pytest==8.4.2 pytest-tap==3.5
+pip install Cython==3.2.4 pytest==8.4.2 pytest-tap==3.5 meson==1.10.0
 
 meson setup /build -Dpython_install=/venv/bin/python
 meson test -C/build --verbose
