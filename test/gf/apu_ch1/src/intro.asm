a430d2fec9b304b0f038be5f845fdbe9bd4d93bf
Gabriel Ford <gabe@fordltc.net>
2026-04-24 13:57:13 -0400

    get tune playing without loop

--- test/gf/apu_ch1/src/intro.asm
+++ test/gf/apu_ch1/src/intro.asm
@@ -5,18 +5,18 @@ INCLUDE "defines.inc"
 
 SECTION "Intro", ROMX
 Intro::
-  ld a, %10001111
-  ldh [rAUDENA], a            ; Turn on sound.
+  ld a, 0b1_000_0_0_0_0 ; [on/off]_[unused]_[RO ch4 on]_[RO ch3 on]_[RO ch2 on]_[RO ch1 on]
+  ldh [rNR52], a        ; Turn on sound.
+
   ld a, $ff
-  ldh [rAUDVOL], a            ; Full volume, both channels on.
-  ldh [rAUDTERM], a           ; All sounds to all terminals.
+  ldh [rNR50], a            ; Full volume, both channels on.
+  ldh [rNR51], a            ; All sounds to all terminals.
   ld a, 0b1000_0000
   ldh [rNR11], a
   ld a, 0b1111_1010
   ldh [rNR12], a
   ld a, 0b1111_1010
   ldh [rNR13], a
-Trigger:
-  ld a, $ff
+  ld a, 0b1_0_000_001 ; [trigger]_[len enable]_[unused]_[period upper]
   ldh [rNR14], a
-  jr Trigger
+  jr @
