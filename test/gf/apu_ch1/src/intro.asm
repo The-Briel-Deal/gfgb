64b8f3432525865e120903b280f63f72d4aad563
Gabriel Ford <gabe@fordltc.net>
2026-04-25 15:31:29 -0400

    get apu_ch1 test rom working

--- test/gf/apu_ch1/src/intro.asm
+++ test/gf/apu_ch1/src/intro.asm
@@ -5,7 +5,7 @@ INCLUDE "defines.inc"
 
 ; Args:
 ;   Period: 11 bit number (16 bit with most significant 5 bits masked off)
-MACRO ch1_set_period
+MACRO ch1_set_period_and_trigger
   DEF period EQU \1
   ld bc, period
   ; Lower 8
@@ -15,6 +15,7 @@ MACRO ch1_set_period
   ; Upper 3
   ld a, b
   and a, 0b0000_0111
+  set 7, a
   ldh [rNR14], a
   PURGE period
 ENDM
@@ -27,17 +28,11 @@ Intro::
   ld a, $ff
   ldh [rNR50], a  ; Full volume, both channels on.
   ldh [rNR51], a  ; All sounds to all terminals.
-  ld a, 0b0000_0000
-  ldh [rNR10], a
   ld a, 0b1000_0000
   ldh [rNR11], a
   ld a, 0b1111_1010
   ldh [rNR12], a
 
-  ch1_set_period 2047
-
-  ; Trigger Channel 1
-  ld  hl, rNR14
-  set 7, [hl]
+  ch1_set_period_and_trigger 512
 
   jr @
