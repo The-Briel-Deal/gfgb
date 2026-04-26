db8009463c62fbb65964e52c934e9baa4a65a611
Gabriel Ford <gabe@fordltc.net>
2026-04-26 16:10:19 -0400

    move gb_apu constructor def to cpp file

--- src/apu.cpp
+++ src/apu.cpp
@@ -1,6 +1,7 @@
 #include "apu.h"
 #include "common.h"
 
+gb_apu_t::gb_apu(gb_state_t &gb_state) : parent(gb_state) { CheckedSDL(Init(SDL_INIT_AUDIO)); }
 void gb_apu_t::update() {
   // TODO: Go through audio registers and play sound accordingly.
 }
