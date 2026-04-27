5bf27b8ff45cd72f008abc053f1705839e5492f5
Gabriel Ford <gabe@fordltc.net>
2026-04-26 20:36:03 -0400

    get simple channel 1 tone playing

--- src/apu.cpp
+++ src/apu.cpp
@@ -28,10 +28,17 @@ static float gb_period_to_tone_freq(uint16_t period) {
 }
 
 void gb_apu_t::update() {
-  io_regs_t &io_regs = this->parent.saved.regs.io;
+  io_regs_t &io_regs       = this->parent.saved.regs.io;
+  bool       ch1_triggered = (io_regs.nr14 >> 7) & 1;
+  if (ch1_triggered) {
+    io_regs.nr52 |= (1 << 0);
+    io_regs.nr14 &= ~(1 << 7);
+  }
+
   // TODO: Go through the rest of the audio registers and play sound accordingly.
   bool apu_powered_on = ((io_regs.nr52 >> 7) & 1);
   if (apu_powered_on) {
+    // TODO: These will need to actually be set by me somewhere since these are read only.
     bool ch1_on = ((io_regs.nr52 >> 0) & 1);
     bool ch2_on = ((io_regs.nr52 >> 1) & 1);
     bool ch3_on = ((io_regs.nr52 >> 2) & 1);
@@ -45,6 +52,32 @@ void gb_apu_t::update() {
       period |= io_regs.nr14;
       period &= MAX_PERIOD;
       uint16_t tone_freq = gb_period_to_tone_freq(period);
+
+      const int minimum_audio = (8000 * sizeof(float)) / 2; /* 8000 float samples per second. Half of that. */
+      if (SDL_GetAudioStreamQueued(this->output_stream) < minimum_audio) {
+        static float samples[2048]; /* this will feed 512 samples each frame until we get to our maximum. */
+        size_t       i;
+
+        /* generate a 440Hz pure tone */
+        for (i = 0; i < SDL_arraysize(samples); i++) {
+          const float phase  = this->current_sine_sample * tone_freq / 8000.0f;
+          float       sample = SDL_sinf(phase * 2 * SDL_PI_F);
+          if (sample >= 0)
+            samples[i] = 1;
+          else
+            samples[i] = -1;
+          this->current_sine_sample++;
+        }
+
+        /* wrapping around to avoid floating-point errors */
+        this->current_sine_sample %= 8000;
+
+        /* feed the new data to the stream. It will queue at the end, and trickle out as the hardware needs more data.
+         */
+        SDL_PutAudioStreamData(this->output_stream, samples, sizeof(samples));
+
+        SDL_ResumeAudioStreamDevice(this->output_stream);
+      }
     }
     if (ch2_on) {
       // TODO: Impl Channel 2
